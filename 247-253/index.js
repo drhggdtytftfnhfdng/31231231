4d059d7424e9ca6b508baec4c981186d5ce46545
drhggdtytftfnhfdng <117073262+drhggdtytftfnhfdng@users.noreply.github.com>
2023-02-07 17:26:31 +0500

    Add files via upload

---
+++ 247-253/index.js
@@ -0,0 +1,129 @@
+// let elem = document.querySelector('#elem');
+// let text = document.querySelector('#text');
+//  elem.addEventListener('click', function() {
+//  	console.log(text.textContent);
+//     });
+// -------------------------------------------2
+// let elem = document.querySelector('#elem');
+//  let text = document.querySelector('#text');
+//  elem.addEventListener('click', function() {
+//   	console.log(text.textContent);
+//    elem.addEventListener('click', function() {
+//  	console.log('1');
+//  });
+//      });
+// -------------------------------------------------3
+// let Botton1 = document.querySelector('#Botton1');
+// let Botton2 = document.querySelector('#Botton2');
+//  let elem1 = document.querySelector('#elem1');
+//  elem.addEventListener('click', function() {
+//   	console.log(Number(bo.textContent) + Number(Botton2.textContent)); 
+//     });
+// ------------------------------------------------------4
+// let door1 = document.querySelector('#door1');
+// let door2 = document.querySelector('#door2');
+// let door3 = document.querySelector('#door3');
+//  let elem1 = document.querySelector('#elem1');
+//  elem1.addEventListener('click', function() {
+//   	console.log(Number(door1.textContent) + Number(door2.textContent) + Number(door3.textContent)); 
+//     });
+// ==========================================================5
+
+// let elem1 = document.querySelector('#elem1');
+//   elem1.addEventListener('click', function() {
+// let text1 = document.querySelector('#text1');
+// console.log(text1.textContent + 5)
+// });
+// ============================================6
+
+// let elem1 = document.querySelector('#elem1');
+//   elem1.addEventListener('click', function() {
+// let text1 = document.querySelector('#text1');
+// console.log(text1.textContent + '!!!')
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
